3c58690e903773efaefb910f567a11b445b7608e
luiz-j <jose.iscosistemas@gmail.com>
2020-04-22 18:13:31 -0700

    Finished yup validation

--- backend/niv03/06-gobarber-web/src/pages/SigIn/index.tsx
+++ backend/niv03/06-gobarber-web/src/pages/SigIn/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
 import logoImg from '../../assets/logo.svg';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 const SigIn: React.FC = () => (
   <Container>
@@ -10,11 +12,16 @@ const SigIn: React.FC = () => (
       <img src={logoImg} alt="" />
       <form>
         <h1>Fala seu logon</h1>
-        <input type="text" placeholder="E-mail" />
+        <Input icon={FiMail} name="email" type="text" placeholder="E-mail" />
 
-        <input type="password" placeholder="Senha" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
---
+++ backend/niv03/06-gobarber-web/src/pages/SignUp/index.tsx
@@ -0,0 +1,57 @@
+import React, { useCallback } from 'react';
+
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import { Container, Content, Background } from './styles';
+import logoImg from '../../assets/logo.svg';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+const SignUp: React.FC = () => {
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome é obrigatório'),
+        email: Yup.string()
+          .required('Email é obrigatorio')
+          .email('Email invalido'),
+        password: Yup.string().min(6, 'Minimo de 6 digitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input icon={FiUser} name="name" type="text" placeholder="Nome" />
+          <Input icon={FiMail} name="email" type="text" placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
